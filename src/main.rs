7709af8ad9ccef98f7555f92f90ea3d9ae18077a
Markus Ullmann <morjgus@protonmail.com>
2022-11-29 15:08:08 +0100

    Add user input

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 // Kaprekar's constant
 
 use itertools::Itertools;
+use std::io;
 
 
 fn main() {
-    let mut val = 123;
+    let mut val = String::new();
+    println!("Please inut a 3 or 4 digits number:");
+    io::stdin().read_line(&mut val)
+        .expect("Failed to read line");
+    let mut val: u32 = val.trim().parse()
+        .expect("Pleae type a number");
 
     loop {
         let val_str = val.to_string();
         let large_chars = val_str.chars().sorted().rev();
         let large_str: String = large_chars.collect::<String>();
-        let large_val: i32 = large_str.parse().unwrap();
+        let large_val: u32 = large_str.parse().unwrap();
         let small_chars = val_str.chars().sorted();
         let small_str: String = small_chars.collect::<String>();
-        let small_val: i32 = small_str.parse().unwrap();
+        let small_val: u32 = small_str.parse().unwrap();
         let next_val = large_val - small_val;
         println!("val: {}, large: {}, small: {}, next: {}", val, large_val, small_val, next_val);
         if next_val == val {
@@ -22,6 +28,10 @@ fn main() {
         val = next_val;
 
     }
-
-    println!("Hello, world!");
+    
+    if val == 495 || val == 6174 {
+        println!("Kaprekar's constant is: {}", val);
+    } else {
+        println!("Couldn't compute Kaprekar's constant. Invalid inpout.");
+    }
 }
